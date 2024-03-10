fc5566cea0995709f9e42fe1eac7c8f63e1511f8
evy568 <2199366892@qq.com>
2024-03-10 21:52:12 +0800

    微调

--- code/src/views.py
+++ code/src/views.py
@@ -22,12 +22,13 @@ def ask():
     # 将文本放在单个消息对象中，用空格分隔不同的文本段落
     message_content = " 以下是一篇高中作文，自命题作文 " \
         f"- 我需要你以一个老师的角色来批阅这篇作文{question} " \
-        "- 完成五个任务，第一个是打分（满分为60），第二个是生成评语，评语既要肯定好的地方也要指出不足，第三个是润色一些语句（给出原句和润色后），第四个是推荐作文想提高可以参照的知识点，第五个是根据这些知识点推荐网课 " \
+        "- 完成五个任务，1.打分（满分为60），2.生成评语，评语既要肯定好的地方也要指出不足，3.润色一些语句（给出原句和润色后，并用换行隔开），4.推荐作文想提高可以参照的知识点，5.根据这些知识点推荐网课 " \
         "- 推荐作文想提高可以参照的知识点部分，比如说如果修辞手法不够，就推荐修辞手法；文章结构不清晰，就推荐文章结构；没有语言风格，就推荐语言风格  " \
         "- The output is just pure JSON format, with no other descriptions." \
         "- Please strictly answer according to the following statements, without extra words. " \
         "- 示例json文件如下，参考格式：[{\"基于文心大模型的打分\": \"\", \"评语\":\"\", \"润色的语句\":\"\",\"可以参照的知识点\": \"\", \"推荐的网课\":\"\",},] "\
         "- 评语在100个中文汉字左右。 " \
+        "- 润色语句至少三句 " \
         "- 推荐作文想提高可以参照的知识点个数在3-5个"
     # 构建对话消息
     messages = [
@@ -54,7 +55,7 @@ def ask():
             json_content = answer[json_start:json_end+1]
             answer_dict = json.loads(json_content)
             # 使用json.dumps进行漂亮的打印
-            print(json.dumps(answer_dict, indent=2, ensure_ascii=False))
+            print(json.dumps(answer_dict, indent=2, ensure_ascii=False).replace("\\n", "\n"))
         else:
             answer_dict = {}
             print("未找到合法的JSON内容。")
